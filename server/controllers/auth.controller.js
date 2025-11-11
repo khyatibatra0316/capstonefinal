0d607eb3df3334545a3166687dd75425d4d7eadc
Khyati Batra <batrakhyati50@gmail.com>
2025-11-12 00:13:30 +0530

    yayayay

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,21 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import userModel from './models/userModel';
+export const register=async(eq,res)=>{
+    const {name, email, password}=req.body
+    if (!name || !email || !password){
+        return res.json({success:false,message:"Missing Details"})
+    }
+    try{
+        const existingUser=await userModel.findOne({email})
+        if(existingUser){
+            return res.json({success:false, message:"User already exists"})
+        }
+        const hashedPassword=await bycrypt.hash(password,10)
+        const user=new userModel ({name , email , password:hashedPassword});
+        await user.save();
+        const token=jwt.sign({id: user._id}, process.env.JWT_SECRET,{expiresIn:'7d'});
+    }catch(error){
+        return res.json({success:false, message:error.message})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieparser from "cookie-parser";
+import connectDB from './config/mongodb.js'
 const app=express();
 const port=process.env.PORT || 4000
 connectDB();
