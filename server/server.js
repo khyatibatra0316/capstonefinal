26671ddb0ac7050bc7538d3c0b5c1ce9366a452c
Khyati Batra <batrakhyati50@gmail.com>
2025-11-11 23:17:41 +0530

    yaya

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ import 'dotenv/config';
 import cookieparser from "cookie-parser";
 const app=express();
 const port=process.env.PORT || 4000
+connectDB();
 app.use(express.json())
 app.use(cookieparser())
 app.use(cors({credentials:true}))
+app.get('/',(req,res)=>res.send("API working"))
 app.listen(port,()=>console.log(`Server is running on PORT:${port}`))
